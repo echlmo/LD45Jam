f518d9dbda136625eb865363ead764a66d37ab54
Tryhardsuckanyway <35487072+Tryhardsuckanyway@users.noreply.github.com>
2019-10-05 12:03:35 +0100

    Added RL player controller modified the GitIgnore, added GitAttributes

---
+++ Offlined/Assets/Scripts/PlayerControllerRL.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerRL : MonoBehaviour
+{
+	
+	CharacterController CharacterController;
+	public float speed = 6.0f;
+	
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+        CharacterController = GetComponent<CharacterController>();
+    }
+	
+	
+    // Update is called once per frame
+    void Update()
+    {
+        CharacterController.Move( new Vector3(Input.GetAxis("Horizontal")*speed*Time.deltaTime,0.0f,0.0f));
+    }
+	
+	
+}
