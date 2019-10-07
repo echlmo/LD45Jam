42b3fd181d6c26c15843560c1b4506d7de2d2dd0
EMo <heylinmo@gmail.com>
2019-10-07 14:14:45 +0100

    prefabs and scripts for vrworld
    added (mostly empty) prefabs for vr world player, enemy spawning and battle encounter and scripts

---
+++ Offlined/Assets/Scripts/GamePlayerStats.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//VR World character stats - not exhaustive or imperative, just some ideas of stats to go off
+public class GamePlayerStats : MonoBehaviour
+{
+
+    public float health;
+    public float mana;
+    public float attack;
+    public float defence;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
